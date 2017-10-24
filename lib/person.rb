ad1f5327403ee7909f763386833488d2b79ed008
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-10-24 14:06:06 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,14 @@
 class Person
 end
 
+def talk
+puts "Hello world!"
+end
+
+def walk
+  puts "The person is walking"
+end
+
 ada = Person.new
 ada = Person.talk
-puts "Hello world!"
-
 ada = Person.walk
-puts "The person is walking"
